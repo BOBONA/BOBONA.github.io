c0fc66d34bbd81534fe043ffadaed3591678a164
Binyamin <binyaminf22@gmail.com>
2021-08-11 18:35:05 -0500

    Contacts added as tables (a contact is defined as whether a user is active during a unique day)

--- chatanalyzer.js
+++ chatanalyzer.js
@@ -3,6 +3,7 @@ function copyToClipboard() {
 }
 
 function appendTable(name, headers, data) {
+    console.log(data);
     let h3 = document.createElement("h3");
     let h3Text = document.createTextNode(name);
     h3.appendChild(h3Text);
@@ -20,7 +21,7 @@ function appendTable(name, headers, data) {
         let row = document.createElement("tr");
         for (let h in headers) {
             let td = document.createElement("td");
-            let text = document.createTextNode(data[i][h]);
+            let text = document.createTextNode(data[i][headers[h]]);
             td.appendChild(text);
             row.appendChild(td);
         }
@@ -32,12 +33,18 @@ function appendTable(name, headers, data) {
 function displayData(data) {
     document.getElementById("output").innerText = JSON.stringify(data, null, 2);
     document.getElementById("copy").style.display = "inline-block";
-    appendTable("Average message counts per month: ",
-                ["Month"].concat(Object.keys(data.months[0][1])),
-                data.months.map((m) => [m[0]].concat(Object.values(m[1]))));
-    appendTable("Message counts per user: ",
-                ["User"].concat(Object.keys(data.users[0][1].messageCountMonthly)),
-                data.users.map((u) => [u[0]].concat(Object.values(u[1].messageCountMonthly))));
+    appendTable("Average message counts per user: ",
+                ["Month", "aveMessage", "aveMessageP50", "aveMessageP75"],
+                data.months.map((m) => Object.assign({}, {"Month": m[0]}, m[1])));
+    appendTable("Message count per user: ",
+                ["User"].concat(Object.keys(data.users[0][1].monthlyMessageCount)),
+                data.users.map((u) => Object.assign({}, {"User": u[0]}, u[1].monthlyMessageCount)));
+    appendTable("Average contact counts per month: ",
+                ["Month", "aveContact", "aveContactP50", "aveContactP75"],
+                data.months.map((m) => Object.assign({}, {"Month": m[0]}, m[1])));
+    appendTable("Contact count per user: ",
+                ["User"].concat(Object.keys(data.users[0][1].monthlyContactCount)),
+                data.users.map((u) => Object.assign({}, {"User": u[0]}, u[1].monthlyContactCount)));
 }
 
 function getLastMonths(count) {
@@ -52,54 +59,76 @@ function getLastMonths(count) {
     return months;
 }
 
+function getAverageInBracket(users, month, sortWith, bracketStart, bracketEnd) {
+    users.sort((u1, u2) => {
+        return u2[1][sortWith][month] - u1[1][sortWith][month];
+    });
+    let sliced = users.slice(bracketStart, Math.floor(users.length * bracketEnd));
+    return Math.round(sliced.reduce((ac, user) => ac + user[1][sortWith][month], 0) / sliced.length);
+}
+
 function getMonth(date) {
-    return (date.getMonth() + 1) + "/" + date.getFullYear()
+    return (date.getMonth() + 1) + "/" + date.getFullYear();
+}
+
+function getDay(date) {
+    return (date.getMonth() + 1) + "/" + date.getFullYear() + "/" + date.getDate();
 }
 
 function process(messages) {
     let monthsList = getLastMonths(12);
-    let messageCountMonthly = {};
+    let monthlyCountObject = {};
     let months = {}
     for (let i in monthsList) {
         let month = monthsList[i];
         months[month] = {
-            average: 0,
-            averagePercentile50: 0,
-            averagePercentile75: 0,
+            aveMessage: 0,
+            aveMessageP50: 0,
+            aveMessageP75: 0,
+            aveContact: 0,
+            aveContactP50: 0,
+            aveContactP75: 0,
         }
-        messageCountMonthly[month] = 0;
+        monthlyCountObject[month] = 0;
     }
     let users = {};
     for (let i in messages) {
         let message = messages[i];
         if (!(message.author in users)) {
             users[message.author] = {
-                messageCountMonthly: {},
+                monthlyMessageCount: {},
+                monthlyContactCount: {},
+                messageDays: {},
             };
-            Object.assign(users[message.author].messageCountMonthly, messageCountMonthly);
+            Object.assign(users[message.author].monthlyMessageCount, monthlyCountObject);
+            Object.assign(users[message.author].monthlyContactCount, monthlyCountObject);
         }
         let month = getMonth(message.date);
-        if (month in users[message.author].messageCountMonthly) {
-            users[message.author].messageCountMonthly[month]++;
-            months[month].average++;
+        if (month in users[message.author].monthlyMessageCount) {
+            users[message.author].monthlyMessageCount[month]++;
+            months[month].aveMessage++;
+            let day = getDay(message.date);
+            if (day in users[message.author].messageDays) {
+                users[message.author].messageDays[day]++;
+            } else {
+                users[message.author].messageDays[day] = 1;
+                users[message.author].monthlyContactCount[month]++;
+                months[month].aveContact++;
+            }
         }
     }
     let userArray = [];
     for (let i in monthsList) {
         let month = monthsList[i];
-        months[month].average = Math.round(months[month].average / Object.keys(users).length);
+        months[month].aveMessage = Math.round(months[month].aveMessage / Object.keys(users).length);
+        months[month].aveContact = Math.round(months[month].aveContact / Object.keys(users).length);
         let userList = Object.keys(users).map((key) => {
             return [key, users[key]];
         });
-        userList.sort((u1, u2) => {
-            return u2[1].messageCountMonthly[month] - u1[1].messageCountMonthly[month];
-        });
-        var sliced50 = userList.slice(0, Math.floor(userList.length * (1 - 0.5)));
-        months[month].averagePercentile50 = Math.round(sliced50
-                                        .reduce((ac, user) => ac + user[1].messageCountMonthly[month], 0) / sliced50.length);
-        var sliced75 = userList.slice(0, Math.floor(userList.length * (1 - 0.75)));
-        months[month].averagePercentile75 = Math.round(sliced75
-                                        .reduce((ac, user) => ac + user[1].messageCountMonthly[month], 0) / sliced75.length);
+        months[month].aveMessageP50 = getAverageInBracket(userList, month, "monthlyMessageCount", 0, 0.5);
+        months[month].aveMessageP75 = getAverageInBracket(userList, month, "monthlyMessageCount", 0, 0.25);
+        months[month].aveContactP50 = getAverageInBracket(userList, month, "monthlyContactCount", 0, 0.5);
+        months[month].aveContactP75 = getAverageInBracket(userList, month, "monthlyContactCount", 0, 0.25);
         if (i == monthsList.length - 1) {
             userArray = userList;
         }
