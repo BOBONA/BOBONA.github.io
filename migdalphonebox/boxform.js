14d7a13c69132ed26b45a59efc2dd261e0e4fe7b
Jacob (Yaakov) <68865880+jzerykier@users.noreply.github.com>
2022-12-04 12:29:00 +0200

    Update boxform.js
    This is the right one.

--- migdalphonebox/boxform.js
+++ migdalphonebox/boxform.js
@@ -1,16 +1,26 @@
-const times = [
-    ["Shacharit", 7, 25],
-    ["Morning+Seder", 8, 50],
-    ["Mincha+%26+Afternoon Seder", 14, 55],
-    ["Night+Seder+%26+Arvit", 19, 10]
-];
-
 const urlParams = new URLSearchParams(window.location.search);
 const inOrOut = urlParams.get("where");
+
+let times;
+if (inOrOut == "IN"){
+    times = [
+        ["Shacharit", 7, 00],
+        ["Morning+Seder", 8, 15],
+        ["Mincha+%26+Afternoon Seder", 13, 00],
+        ["Night+Seder+%26+Arvit", 18, 40]
+    ];
+} else {
+    times = [
+        ["Shacharit", 7, 00],
+        ["Morning+Seder", 11, 30],
+        ["Mincha+%26+Afternoon Seder", 16, 00],
+        ["Night+Seder+%26+Arvit", 20, 20]
+    ];
+}
+
 const phones = JSON.parse(urlParams.get("phones"));
 const apartment = urlParams.get("apartment");
 const name = urlParams.get("name");
-
 const now = new Date();
 const minutes = now.getHours() * 60 + now.getMinutes();
 let time = times[times.length - 1][0];
@@ -22,7 +32,6 @@ for (let i = 0; i < times.length - 1; i++) {
         break;
     }
 }
-
 const ids = {
     "39": "570713596",
     "49": "865593227",
@@ -31,6 +40,5 @@ const ids = {
     "35": "237174216",
     "14": "1319684264"
 }
-
 window.location.replace(`https://docs.google.com/forms/d/e/1FAIpQLScTZluOOu2J87HRLBj7FZ_zVqZpZQ8Az9YtEiUaEzlcI3tLjg/formResponse?&submit=Submit&` +
     `entry.275193204=${time}&entry.1682337327=${inOrOut}&entry.2096638216=${phone ? "Yes" : "No"}&entry.115186426=${apartment}&entry.${ids[apartment]}=${name}`);
